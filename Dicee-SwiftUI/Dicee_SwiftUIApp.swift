eb91be3e937a204b3e13bc391761bf3ebc0b7aa4
kleeCollage <klee.collage@gmail.com>
2026-01-10 22:08:54 -0600

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Antonio Hernández Santander on 10/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Dicee-SwiftUITests/Dicee_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUITests.swift
+//  Dicee-SwiftUITests
+//
+//  Created by Antonio Hernández Santander on 10/01/26.
+//
+
+import Testing
+@testable import Dicee_SwiftUI
+
+struct Dicee_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
