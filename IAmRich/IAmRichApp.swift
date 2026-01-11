cb548dd146ee51d0c07effb7ee84ae57a65b1d85
kleeCollage <klee.collage@gmail.com>
2026-01-10 18:15:35 -0600

    Initial Commit

---
+++ IAmRich/IAmRichApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRichApp.swift
+//  IAmRich
+//
+//  Created by Antonio Hernández Santander on 10/01/26.
+//
+
+import SwiftUI
+
+@main
+struct IAmRichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IAmRichTests/IAmRichTests.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRichTests.swift
+//  IAmRichTests
+//
+//  Created by Antonio Hernández Santander on 10/01/26.
+//
+
+import Testing
+@testable import IAmRich
+
+struct IAmRichTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
