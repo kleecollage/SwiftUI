2d23c2f5ddc1e2fb60449dd38876cc4dd3c5caf9
kleeCollage <klee.collage@gmail.com>
2026-01-12 13:08:19 -0600

    feat: Implement navigation view

---
+++ H4XOR-News/Views/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  H4XOR-News
+//
+//  Created by Antonio Hernández Santander on 12/01/26.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    DetailView(url: "https://www.google.com")
+}
